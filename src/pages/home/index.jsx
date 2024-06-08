5dc80066d4d2df9555f1eb8b01cacf6c2efeef77
Zoedube <rofhiwazoe11@gmail.com>
2024-06-08 19:47:54 +0200

    new pages

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../../App.css';
+import './home.css';
 import Cards from '../Cards';
 
 function Home() {
   return (
     <>
+    <div className="home">
       <Cards />
+      </div>
     </>
   );
 }
