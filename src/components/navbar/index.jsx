1e5c827601f4e754f88daf65911ceff132ae8cbe
Zoedube <rofhiwazoe11@gmail.com>
2024-06-09 18:38:03 +0200

    uodated tailwind code

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   // Code to create navbar
   return (
     <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
-      <h2 className="text-2xl font-semibold">
+      <h2 className="text-4xl font-semibold">
         <NavLink
             to={"/"}
             className="text-black hover:text-gray-700 duration-300" >
@@ -19,7 +19,7 @@ export default function Navbar() {
           className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-pink-100 focus:shadow-pink-200"
         />
       </form>
-      
+
       <ul className="flex gap-5">
         <li>
           <NavLink
