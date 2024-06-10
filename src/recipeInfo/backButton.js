ece1dda31aa962232cd8e2d25ffd0cc0c841ed66
lethabomichelle <163848629+lethabomichelle@users.noreply.github.com>
2024-06-10 11:27:08 +0200

    Update backButton.js

--- src/recipeInfo/backButton.js
+++ src/recipeInfo/backButton.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"; // Import Link for routing
 
 const BackButton = () => {
     return (
-        <Link to="/"> {/* Replace "/" with the path to your previous page */}
+        <Link to="/"> {}
             <button className="bg-[#C3C6D5]  text-black font-bold py-2 px-4 rounded">
                 Back
             </button>
